1c538fcae6b8fd43a0f24d9263dcedae54e62b6d
Cindey Caine <ccaine@ucsd.edu>
2018-02-27 16:58:23 -0800

    pushing demo for react

---
+++ demo/src/components/common/Header.js
@@ -0,0 +1,18 @@
+import React, {PropTypes} from 'react';
+import { Link, IndexLink } from 'react-router';
+import LoadingDots from './LoadingDots';
+
+const Header = ({loading}) => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active"><img class="logo" src="car_logo.png" alt="logo"></img></IndexLink>
+      {loading && <LoadingDots interval={100} dots={20}/>}
+    </nav>
+  );
+};
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
+};
+
+export default Header;
---
+++ demo/src/components/startnegotiation/StartNegotiationPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class StartNegotiationPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Start Negotiation</h1>
+        <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
+      </div>
+    );
+  }
+}
+
+export default StartNegotiationPage;
