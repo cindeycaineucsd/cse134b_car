dfd8a27285ead4aafba3d71750055dcb792d44c8
sean <s1powers@ucsd.edu>
2018-03-01 11:19:14 -0800

    simple login and sign up

--- demo/src/components/common/Header.js
+++ demo/src/components/common/Header.js
@@ -7,15 +7,128 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import Toggle from 'material-ui/Toggle';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import Dialog from 'material-ui/Dialog';
 
-class Login extends React.Component {
+const logsignstyle = {
+  marginTop: 5, color: '#FFFFFF'
+};
+
+class LogSign extends React.Component {
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+      loginOpen: false,
+      signupOpen: false
+    };
+
+    this.handleLoginOpen = this.handleLoginOpen.bind(this);
+    this.handleLoginClose = this.handleLoginClose.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleSignUpOpen = this.handleSignUpOpen.bind(this);
+    this.handleSignUpClose = this.handleSignUpClose.bind(this);
+    this.handleSignUpSubmit = this.handleSignUpSubmit.bind(this);
+
+  }
+
+  handleLoginOpen(){
+    this.setState({loginOpen: true});
+  }
+
+  handleLoginClose(){
+    this.setState({loginOpen: false});
+  }
+
+  handleLoginSubmit(){
+    this.setState({loginOpen: false});
+  }
+
+  handleSignUpOpen(){
+    this.setState({signupOpen: true});
+  }
+
+  handleSignUpClose(){
+    this.setState({signupOpen: false});
+  }
+
+  handleSignUpSubmit(){
+    this.setState({signupOpen: false});
+  }
 
   render() {
+      const loginActions = [
+        <FlatButton
+          label="Cancel"
+          primary={true}
+          onClick={this.handleLoginClose}
+        />,
+        <FlatButton
+          label="Submit"
+          primary={true}
+          keyboardFocused={true}
+          onClick={this.handleLoginSubmit}
+        />
+      ];
+
+      const signupActions = [
+        <FlatButton
+          label="Cancel"
+          primary={true}
+          onClick={this.handleSignUpClose}
+        />,
+        <FlatButton
+          label="Submit"
+          primary={true}
+          keyboardFocused={true}
+          onClick={this.handleSignUpSubmit}
+        />
+      ];
+
     return (
-      <FlatButton {...this.props} label="Login" />
+      <div>
+        <FlatButton {...this.props} label="SignUp" secondary="true" style={logsignstyle} onClick={this.handleSignUpOpen}/>
+        <Dialog
+          title="Sign Up"
+          actions={signupActions}
+          modal={false}
+          open={this.state.signupOpen}
+          onRequestClose={this.handleSignUpClose}
+        >
+          <label><b>Username</b></label>
+          <input id="signup_username" type="text" placeholder="Enter Username" name="username" required />
+
+          <label><b>Password</b></label>
+          <input  id="signup_password" type="text" placeholder="Enter Password" name="password" required />
+
+          <label><b>Confirm Password</b></label>
+          <input  id="signup_password2" type="text" placeholder="Re-Enter Password" name="password_conf" required />
+
+          <div>
+            <label><b>Select User Type</b></label>
+          </div>
+          <div>
+            <input id="buyer_radio" type="radio" name="usertype" value="buyer" /> Buyer <br />
+            <input id="seller_radio" type="radio" name="usertype" value="seller" /> Seller <br />
+          </div>
+        </Dialog>
+        <FlatButton {...this.props} label="Login" secondary="true" style={logsignstyle} onClick={this.handleLoginOpen}/>
+        <Dialog
+          title="Login"
+          actions={loginActions}
+          modal={false}
+          open={this.state.loginOpen}
+          onRequestClose={this.handleLoginClose}
+        >
+        <label><b>Username</b></label>
+        <input id="login_username" type="text" placeholder="Enter Username" name="username" required />
+
+        <label><b>Password</b></label>
+        <input id="login_password" type="text" placeholder="Enter Password" name="password" required />
+        </Dialog>
+      </div>
     );
   }
 }
@@ -41,7 +154,7 @@ class Header extends React.Component {
   constructor(props, context){
     super(props, context);
     this.state = {
-      logged: true
+      logged: false
     };
     
     this.handleChange = this.handleChange.bind(this);
@@ -57,7 +170,7 @@ class Header extends React.Component {
       <div>
         <AppBar
           title="Car-2-Go"
-          iconElementRight={this.state.logged ? <Logged /> : <Login />}
+          iconElementRight={this.state.logged ? <Logged /> : <LogSign />}
         />
       </div>
     );
--- demo/src/components/startnegotiation/StartNegotiationPage.js
+++ demo/src/components/startnegotiation/StartNegotiationPage.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
+
+const fullpaperstyle = {
+  width: "100%",
+  minWidth: "250px",
+  height: "100%",
+  paddingTop: "100px",
+  backgroundColor: "#EFEFEF",
+  margin: "0px auto"
+};
+
+const simplecardstyle = {
+  width: "75%", margin: "0px auto", minWidth: "200px", maxWidth: "750px"
+}
+
+const CardExampleWithAvatar = () => (
+  <Card style={simplecardstyle}>
+    <CardTitle title="Start Negotiation?"/>
+    <CardActions>
+      <RaisedButton label="Yes" primary="true"/>
+      <RaisedButton label="No" primary="true"/>
+    </CardActions>
+  </Card>
+);
 
 class StartNegotiationPage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Start Negotiation</h1>
-        <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
+      <div style={fullpaperstyle}>
+          <CardExampleWithAvatar/>
       </div>
     );
   }
